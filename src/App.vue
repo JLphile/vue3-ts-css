b116496ba1bd16523fca2344a393b0d74d894c56
JLphile <lkyz001@outlook.com>
2022-09-29 23:18:21 +0800

    feat(main): 局部引入组件 局部引入样式
    import 'element-plus/es/components/button/style/css'

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,26 @@
     <router-link to="/login">登录</router-link>
     <router-link to="/main">首页</router-link>
     <router-view></router-view>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
+// element-plus局部引入组件并注册
+import { ElButton } from 'element-plus'
+
+// element-plus局部引入样式方法1
+// import 'element-plus/es/components/button/style/css'
+// element-plus局部引入样式方法2
+// import 'element-plus/theme-chalk/base.css'
+// import 'element-plus/theme-chalk/el-button.css'
+
 export default defineComponent({
   name: 'App',
-  components: {}
+  components: { ElButton }
 })
 </script>
 
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 全局引入组件
+// import ElementPlus from 'element-plus'
+
+// 全局引入样式
+// import 'element-plus/dist/index.css'
+
 const app = createApp(App)
+
 app.use(router)
 app.use(store)
+
+// app.use(ElementPlus)
+
 app.mount('#app')
