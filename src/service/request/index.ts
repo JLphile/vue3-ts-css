0e0a2607cc75fd2f99ec28aa1ea266da0f4dd96a
JLphile <lkyz001@outlook.com>
2022-10-03 22:42:39 +0800

    feat(main): interceptors to evryone
    this.instance.interceptors.request.use this.instance.interceptors.response.use

--- src/service/request/index.ts
+++ src/service/request/index.ts
@@ -6,10 +6,12 @@ class HYRequest {
   instance: AxiosInstance
   interceptors?: HYRequestInterceptors
 
+  // 此处用HYRequestConfig将axios原生AxiosRequestConfig类型进行了拓展
   constructor(config: HYRequestConfig) {
     this.instance = axios.create(config)
     this.interceptors = config.interceptors
 
+    // 从config中取出的拦截器时对应的实例的拦截器
     this.instance.interceptors.request.use(
       this.interceptors?.requestInterceptor,
       this.interceptors?.requestInterceptorCatch
@@ -17,6 +19,30 @@ class HYRequest {
     this.instance.interceptors.response.use(
       this.interceptors?.responseInterceptor,
       this.interceptors?.responseInterceptorCatch
+    )
+
+    // 添加所有的实例的拦截器
+    this.instance.interceptors.request.use(
+      (config) => {
+        console.log('所有的实例都有的拦截器：请求成功拦截')
+
+        return config
+      },
+      (err) => {
+        console.log('所有的实例都有的拦截器：请求失败拦截')
+        return err
+      }
+    )
+
+    this.instance.interceptors.response.use(
+      (res) => {
+        console.log('所有的实例都有的拦截器：响应成功拦截')
+        return res
+      },
+      (err) => {
+        console.log('所有的实例都有的拦截器：响应失败拦截')
+        return err
+      }
     )
   }
 
