068031bab4ee59036e31ae23ef4eba4fdd6afb85
Jlphile <lkyz001@outlook.com>
2022-10-09 10:08:34 +0800

    feat(v35.0.1): v35-00:26:36
    登陆逻辑-跨域问题

--- src/store/login/login.ts
+++ src/store/login/login.ts
@@ -1,5 +1,7 @@
 import { Module } from 'vuex'
 
+import { IAccount } from '@/service/login/type'
+import { accountLoginRequest } from '@/service/login/login'
 import { ILoginState } from './types'
 import { IRootState } from '../types'
 
@@ -15,12 +17,15 @@ const loginModule: Module<ILoginState, IRootState> = {
   getters: {},
   mutations: {},
   actions: {
-    accountLoginAction({ commit }, payload: any) {
-      console.log('执行了accountLoginAction', payload)
-    },
-    phoneLoginAction({ commit }, payload: any) {
-      console.log('执行了phoneLoginAction')
+    async accountLoginAction({ commit }, payload: IAccount) {
+      // 1.实现登陆逻辑
+      const loginResult = await accountLoginRequest(payload)
+      console.log(loginResult)
     }
+
+    // phoneLoginAction({ commit }, payload: any) {
+    //   console.log('执行了phoneLoginAction')
+    // }
   }
 }
 
